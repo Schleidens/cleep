d4e7e2e59031f70a1b725428d62959a10755f67c
Schleidens <alectineschleidens2016@gmail.com>
2023-08-24 15:33:07 -0400

    basic redirect logic from signup page if user is authenticated

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,30 +1,19 @@
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import { Navigate } from "react-router-dom";
 
-import { auth } from "../firebase/main";
-import { handleSignInWithGoogle } from "../firebase/authentication"
-
+import { handleSignInWithGoogle } from "../firebase/authentication";
 
 function SignupPage() {
+  const isUserAuthenticated = localStorage.getItem("token");
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    const uid = user.displayName;
-    console.log(uid);
-  } else {
-    // User is signed out
-    console.log("no user");
+  if (isUserAuthenticated) {
+    return <Navigate to="/" />;
   }
-});
-  }, [])
 
   return (
     <>
       <button onClick={handleSignInWithGoogle}>Google</button>
     </>
-  )
+  );
 }
 
-export default SignupPage
+export default SignupPage;
