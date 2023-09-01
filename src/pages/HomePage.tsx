8a0e6641c06b84a663f8fbcc8145ce79c57e0361
Schleidens <alectineschleidens2016@gmail.com>
2023-09-01 15:47:54 -0400

    make ui for the homePagr

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import { handleSignOut } from "../firebase/authentication"
+
+import style from "../styles/homepage.module.scss"
+
+import Notes from "../components/notes";
+
+import { IoIosDocument } from "react-icons/io"
+import { BiLogOutCircle, BiPlusCircle } from "react-icons/bi"
 
 const HomePage: React.FC = () => {
 
@@ -19,10 +28,40 @@ const HomePage: React.FC = () => {
   }
 
   return (
-    <div>
-        CLEEP
+    <div className={style.main}>
+        <div className={style.main__sidebar}>
+          <div>
+            <div className={style.logo}>
+              <Link to="/"><IoIosDocument /></Link>
+            </div>
 
-        <button onClick={signOutUser}>Sign Out</button>
+            <div>
+              <button className={style.plus}>
+                <BiPlusCircle />
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <button onClick={signOutUser} className={style.logout}><BiLogOutCircle /></button>
+          </div>
+        </div>
+
+        <div className={style.main__box}>
+          <div className={style.brand}>
+            <h1>
+              CLEEP
+            </h1>
+          </div>
+
+          <div className={style.notes}>
+            <div className={style.header}><h2>Notes</h2></div>
+
+            <div className={style.notes__box}>
+              <Notes />
+            </div>
+          </div>
+        </div>
 
         <div>
           {errorMessage}
