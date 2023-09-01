df95d9c72aac9233e2fedfbee171b9f48e742711
Schleidens <alectineschleidens2016@gmail.com>
2023-09-01 15:47:22 -0400

    done signup page

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,12 +1,18 @@
-import { handleSignInWithGoogle } from "../firebase/authentication";
 import { useState } from "react";
+import { handleSignInWithGoogle } from "../firebase/authentication";
+
+import style from "../styles/signup.module.scss"
+
+import { FcGoogle } from "react-icons/fc"
 
 const SignupPage: React.FC = () => {
 
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
   const handleSignIn = async (): Promise<void> => {
     try {
+      setIsDisabled(true)
       await handleSignInWithGoogle();
     } catch (error) {
       if(error instanceof Error){
@@ -14,14 +20,24 @@ const SignupPage: React.FC = () => {
       }else{
         setErrorMessage("Something's wrong try again :)")
       }
+
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    } finally{
+      setIsDisabled(false)
     }
   }
 
   return (
     <>
-      <button onClick={handleSignIn}>Google</button>
-
-      <span>{errorMessage}</span>
+      <div className={style.main}>
+        <div onClick={handleSignIn} className={style.main__button} style={isDisabled ? {opacity: '0.5', pointerEvents: 'none'} : {}}>
+          {!isDisabled && <div><FcGoogle /> <div>SignUp with Google</div></div>}
+          {isDisabled && <div>SignUp.....</div>}
+        </div>
+        {errorMessage && <div className={style.main__error}>{errorMessage}</div>}
+      </div>
     </>
   );
 }
