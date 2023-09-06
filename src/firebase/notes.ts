530eafd08472c80aca8c6352efc6b87116f18ba6
Schleidens <alectineschleidens2016@gmail.com>
2023-09-06 16:09:35 -0400

    create export function for create and fetch notes

--- src/firebase/notes.ts
+++ src/firebase/notes.ts
@@ -1,14 +1,49 @@
 import { db } from "./main";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, orderBy, where } from "firebase/firestore";
+
+import { noteDataModel } from "../ts/noteDataModel";
 
 
-export const createNotes = async ( noteData: unknown): Promise<void> => {
+export const createNotes = async ( noteData: noteDataModel): Promise<void> => {
     try {
-    const docRef = await addDoc(collection(db, 'notes'), { noteData });
+    const docRef = await addDoc(collection(db, 'notes'), noteData);
 
     console.log('Note created successfully with ID: ', docRef.id);
   } catch (error) {
     console.error('Error creating note: ', error);
+    throw error;
+  }
+}
+
+
+
+export const getNotes = async (userId: unknown): Promise<noteDataModel[]> => {
+  try {
+    const notesCollection = collection(db, 'notes');
+    const orderedQuery = query(notesCollection, orderBy('lastEdit', 'desc'), where('userId', '==', userId));
+    const querySnapshot = await getDocs(orderedQuery);
+
+    const allNotes: noteDataModel[] = [];
+
+    querySnapshot.forEach((doc) => {
+      // Add each document's data to the array
+      const data = doc.data();
+      
+      if (data) {
+        allNotes.push({
+        id: doc.id,
+        title: data.title || '',
+        content: data.content || '',
+        color: data.color || '',
+        userId: data.userId || '',
+        lastEdit: data.lastEdit || '',
+      })
+      }
+    });
+
+    return allNotes;
+  } catch (error) {
+    console.error('Error while fetching documents: ', error);
     throw error;
   }
 }
