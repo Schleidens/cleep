ffe1c4845155da3ced1c2b6e70157c870b9ffe1b
Schleidens <alectineschleidens2016@gmail.com>
2023-09-06 16:09:58 -0400

    add type for notes

---
+++ src/ts/noteDataModel.ts
@@ -0,0 +1,8 @@
+export interface noteDataModel {
+  id: string | undefined;
+  title: string | undefined;
+  content: string | undefined;
+  color: string | undefined;
+  userId: string | undefined;
+  lastEdit: string | number | Date;
+}
