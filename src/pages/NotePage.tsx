aebd3fd4dc3e215866bfa7e08944c5089892a570
Schleidens <alectineschleidens2016@gmail.com>
2023-10-14 15:06:37 -0400

    add delete button and delete feature to delete a single note

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,22 +1,27 @@
-import { useCallback, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useCallback, useEffect, useState } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
-import style from "../styles/notepage.module.scss";
-import { IoMdArrowRoundBack } from "react-icons/io";
+import style from '../styles/notepage.module.scss';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+import { MdOutlineDeleteOutline } from 'react-icons/md';
 
-import NotePageSkeleton from "../components/notePageSkeleton";
+import NotePageSkeleton from '../components/notePageSkeleton';
 
-import { getSingleNote } from "../firebase/notes";
+import { getSingleNote, deleteSingleNote } from '../firebase/notes';
 
-import { singleNoteDataModel } from "../ts/noteDataModel";
-import { formatDate } from "../ts/formatDate";
+import { singleNoteDataModel } from '../ts/noteDataModel';
+import { formatDate } from '../ts/formatDate';
 
 const NotePage: React.FC = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   //loading statement
   const [loading, setLoading] = useState<boolean>(false);
+  //note
   const [note, setNote] = useState<singleNoteDataModel | null>(null);
+
+  //note fetching function
   const fetchSingleNote = useCallback(async (): Promise<void> => {
     try {
       setLoading(true);
@@ -32,13 +37,31 @@ const NotePage: React.FC = () => {
     }
   }, [id]);
 
+  //delete note function
+  const deleteNote = async (): Promise<void> => {
+    try {
+      await deleteSingleNote(id);
+      //redirect to home
+      navigate('/');
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error);
+      } else {
+        console.log('Error while deleting note');
+      }
+    }
+  };
+
   useEffect(() => {
     fetchSingleNote();
   }, [fetchSingleNote]);
   return (
     <div className={style.main}>
       {!loading && (
-        <Link to="/" className={style.back}>
+        <Link
+          to='/'
+          className={style.back}
+        >
           <IoMdArrowRoundBack />
         </Link>
       )}
@@ -52,7 +75,12 @@ const NotePage: React.FC = () => {
           <h1>{note?.title}</h1>
           <div className={style.content}>{note?.content}</div>
           <div className={style.footer}>
-            {note && <span>{formatDate(note.lastEdit)}</span>}
+            {note && <span>{formatDate(note.lastEdit)}</span>}{' '}
+            <span>
+              <button onClick={deleteNote}>
+                <MdOutlineDeleteOutline />
+              </button>
+            </span>
           </div>
         </div>
       )}
